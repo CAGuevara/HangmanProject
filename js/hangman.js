674a1f878cb9f53f46facf9367a0800b4dccfa72
Carmen Guevara <carmen.guevara75@gmail.com>
2021-12-22 11:09:47 -0500

    Second Push

--- js/hangman.js
+++ js/hangman.js
@@ -1,86 +1,118 @@
 // test 1 
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+// let showClue = document.getElementById("clue");
+const showClue = document.querySelector("#clue");
+const alphaContainer = document.querySelector("#alphaContainer");
+const playAgain = document.querySelector("#playAgain");
+const category = document.querySelector("#categories");
+
+alphabet.forEach(letter => {
+
+})
+
 
 let occupations =[
     {
     name : "Librarian",
-    Hint : "Works at a Library"
+    Hint : "Works at a Library",
+    cat : "Occupations"
     },
     {
     name : "Newscaster",
-    hint : "reads news"    
+    hint : "reads news",
+    cat : "Occupations"    
     },
     {
     name : 'Flight Attendant',
-    hint : 'works on the air'
+    hint : 'works on the air',
+    cat : "Occupations"
     },
     {
     name :"Reporter",
-    hint : "Cover News"
+    hint : "Cover News",
+    cat : "Occupations"
     },
     {
     name: "Waiter",
-    hint : 'Works at a restaurant'
+    hint : 'Works at a restaurant',
+    cat : "Occupations"
     }
 ]
 let animals = [
     {
         name : "Jiraffe",
-        hint : "long neck"
+        hint : "long neck",
+        cat : "Animals"
     },
     {
         name : "Elephant",
-        hint : "Heavy"
+        hint : "Heavy",
+        cat : "Animals"
     },
     {
         name : "Cheetah",
-        hint : "Always Laughing"
+        hint : "Always Laughing",
+        cat : "Animals"
     },
     {
         name : "Bear",
-        hint : "Don't run from them"
+        hint : "Don't run from them",
+        cat : "Animals"
     },
     {
         name : "Shark",
-        hint : "they live in the Ocean"
+        hint : "they live in the Ocean",
+        cat : "Animals"
     }
 ]
 
 let famousCharacters = [
     {
        name : 'Groot',
-       hint : 'Bradley Cooper'
+       hint : 'Bradley Cooper',
+       cat : "famous Characters"
     },
     {
         name : 'Edna Mode',
-        hint : 'The Incredibles'
+        hint : 'The Incredibles',
+        cat : "famous Characters"
     },
     {
         name: 'Optimus Prime',
-        hint : "Tranformers"
+        hint : "Tranformers",
+        cat : "famous Characters"
     },
     {
         name : 'The Minions',
-        hint : 'In their language Bu ta na ma ka means : To Infinity and Beyond'
+        hint : 'In their language Bu ta na ma ka means : To Infinity and Beyond',
+        cat : "famous Characters"
     }
 ]
 const categories = [occupations,animals,famousCharacters];
-// console.log(occupations[0].name);
+// console.log(occupations[1]);
 
 let final = getWord();
+category.textContent = final.cat;
 console.log(final.name);
 console.log(final.hint);
+console.log(final.cat);
 
-const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+hint.onclick = function(){
+    showClue.innerHTML=`${final.hint}`;  
+    hint.style.visibility ="hidden"; 
+};
 
 
 
-
-// test 2
-// const ocupations = ['librarian','newscaster','Flight Attendant','Reporter','Waiter'];
-// const animals =['Bear','Cheetah','Shark','Jiraffe', 'Elephant'];
-// const famousCharacters = ['Groot','Edna Mode','Optimus Prime','The Minions']
-// const categories = [ocupations,famousCharacters,animals];
-
+playAgain.onclick = function(){
+    // console.log(getWord());
+    final = getWord();
+    showClue.innerHTML = " ";
+    // console.log(final)
+    hint.style.visibility ="visible";
+    category.textContent = final.cat;
+  
+}
 
 function getRandom (max){
     return Math.floor(Math.random()* max);
@@ -88,34 +120,12 @@ function getRandom (max){
 
 function getWord () {
     const randomCat = getRandom(categories.length);
+    console.log(randomCat);
     const randomSub = getRandom(categories[randomCat].length);
     const guessWord = categories[randomCat][randomSub];
-    // console.log(guessWord);
     return guessWord;
 }
 
 
-
-
-
-
-//  let ranLetter = getRandom(alphabet.length);
-//  console.log(ranLetter);
-function findLetter(){
-    // const x = getWord();
-    const x = 'anaconda'
-    console.log(x);
-    const letter = 'a'
-    // const found = 'anaconda'.indexOf(letter)
-    // console.log(found);
-    for (let i=0;i<x.length;i++){
-        console.log(x[i]);
-        if (letter===x[i]){
-            console.log(`position of letter ${letter} is ${i}`)
-        }
-    }
-    
-}
-// findLetter();
 
 
